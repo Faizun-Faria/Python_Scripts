59d82e17bbf4ae893d16657472ccb10cfa81bb6f
Faizun-Faria <34593750+Faizun-Faria@users.noreply.github.com>
2020-06-25 18:17:48 +0600

    Update whatsapp_auto_message_n_times.py

--- whatsapp_auto_message_n_times.py
+++ whatsapp_auto_message_n_times.py
@@ -5,25 +5,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import sys
+
 driver = webdriver.Chrome(r'C:\Drivers\chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
-# Replace 'My Bsnl' with the name of your friend or group name
-target = '"Sabi"'
-
-# Replace the below string with your own message
-string = "my besty "
-
+target = '"Mom"'
+text = "I love you mommy"
 x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-By.XPATH, x_arg)))
-print (group_title)
-print ("Wait for few seconds")
+group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
 group_title.click()
 message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-for i in range(500):
-    message.send_keys(string)
+for i in range(50):
+    message.send_keys(text)
     sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
     sendbutton.click()
 driver.close()
